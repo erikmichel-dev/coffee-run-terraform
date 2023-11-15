265e12bb31e068a3eca30c46957d1b0a1c15785a
Erik Michel <erikmichelrobado@gmail.com>
2023-11-15 20:40:56 +0100

    added tier list table

--- modules/dynamo_db/main.tf
+++ modules/dynamo_db/main.tf
@@ -13,3 +13,14 @@ resource "aws_dynamodb_table" "coffee_pool" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "tier_list" {
+  name         = "tier_list-${var.infra_env}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "tier_id"
+
+  attribute {
+    name = "tier_id"
+    type = "S"
+  }
+}
