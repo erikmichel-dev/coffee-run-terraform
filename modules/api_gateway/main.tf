14c29460c7821ef81e075e4dbdfaa2a718aef09b
Erik Michel <erikmichelrobado@gmail.com>
2023-12-06 17:42:11 +0100

    added triggers on stage deployment for all methods and integrations

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -124,7 +124,13 @@ resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
 
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.this.body))
+    redeployment = sha1(join("", [
+      jsonencode(aws_api_gateway_rest_api.this.body),
+      jsonencode(aws_api_gateway_method.get_daily_coffee),
+      jsonencode(aws_api_gateway_integration.get_daily_coffee),
+      jsonencode(aws_api_gateway_method.opt_daily_coffee),
+      jsonencode(aws_api_gateway_integration.opt_daily_coffee)
+    ]))
   }
 
   lifecycle {
