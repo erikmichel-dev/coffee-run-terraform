7ce1f27c34bc690c895c91c6c4f85e84a7480807
Erik Michel <erikmichelrobado@gmail.com>
2023-12-10 01:03:24 +0100

    added s3 bucket hosting and cloudfront distribution

---
+++ modules/s3_hosting/variables.tf
@@ -0,0 +1,11 @@
+variable "infra_env" {
+  description = "Current environment"
+}
+
+variable "region" {
+  description = "Region used"
+}
+
+variable "s3_name" {
+  description = "S3 bucket name"
+}
